2d0b1aeb17b226543650c7341dc4274a6b3430b8
Nandhika26 <nandikadiega@gmail.com>
2024-05-08 14:09:52 +0700

    Routing and View

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Media </title>
+    <link rel="stylesheet" type="text/css" href="laravel-projek/resources/css/home.css">
+</head>
+<style>body {
+    font-family: Arial, sans-serif;
+    background-color: #DADDB1;
+    color: #ffffff;
+    margin: 0;
+    padding: 0;
+}
+ 
+h1 {
+    font-size: 36px;
+    color: #B3A492;
+    text-align: center;
+}
+
+h2 {
+    font-size: 24px;
+    color: #000000;
+    text-align: center;
+}
+
+p {
+    font-size: 16px;
+    color: #555843;
+    text-align: center;
+}
+
+ul, ol {
+    list-style: none;
+    color: #555843;
+    text-align: center;
+}
+
+ul li, ol li {
+    margin: 10px 0;
+}
+
+a {
+    text-decoration: none;
+    color: #ffa500;
+    text-align: center;
+}
+
+a:hover {
+    color: #ffcc00;
+}
+</style>
+<body>
+    <h1> Media Online </h1>
+    <h2> Sosial Media Developer </h2>
+    <p> Belajar dan berbagi agar hidup menjadi lebih baik </p>
+    <h2> Benefit Join di Media Online </h2>
+    <ul>
+            <li>Mendapatkan motivasi dari sesama para Developer</li>
+            <li>Sharing knowledge</li>
+            <li>Dibuat oleh calon Developer terbaik</li>
+    </ul>
+        <h2>Cara Bergabung ke Media Online</h2>
+        <ol type="1">
+            <li>Mengunjungi Website ini</li>
+            <li>Mendaftarkan di <a href="form.blade.php">Form Sign UP</a></li>
+            <li>Selesai</li>
+        </ol>
+</body>
+</html>
